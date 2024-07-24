6bb58248d9ea329244100966016537b5551e68a0
LucasAlAlam <lucasalalam@ejcm.com.br>
2024-07-24 17:04:11 -0300

    feat: routes

--- ZoeTrope modelo/Backend/src/routes/routes.ts
+++ ZoeTrope modelo/Backend/src/routes/routes.ts
@@ -1,1 +1,12 @@
+import userController from "../controllers/userController";
+import { Router } from "express";
 
+const routes = Router();
+
+routes.post('/users', userController.create);
+routes.get('/users', userController.index);
+routes.get ('/users/search', userController.show);
+routes.put('/users/:id', userController.update);
+routes.delete('/users/:id', userController.delete);
+
+export default routes;
